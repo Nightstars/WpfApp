128ff8f48cc36fc7126376974b09a0c79c95818c
Nightstars <christzhangowner#gmail.com>
2021-05-09 16:45:05 +0800

    添加项目文件。

---
+++ WpfApp/Users/UserWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp.Users
+{
+    /// <summary>
+    /// UserWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        public UserWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string uname = username.Text.Trim();
+            string upwd = password.Password.Trim();
+            MessageBox.Show($"登录成功,{uname} {upwd}", "登录提示", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
